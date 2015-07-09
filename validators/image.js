d5cf57799dad050044aa347b817bd010cd80f97e
Sendy Halim <sendyhalim93@gmail.com>
2015-07-09 15:33:53 +0700

    Check if given value is object (for image rule), if its an object then get the path

--- validators/image.js
+++ validators/image.js
@@ -1,9 +1,14 @@
 var readChunk = require('read-chunk');
 var imageType = require('image-type');
+var _ = require('lodash');
 
 exports = module.exports = {
   validator: function(val) {
     if (val) {
+      if (_.isObject(val)) {
+        val = val.path;
+      }
+
       var buffer = readChunk.sync(val, 0, 12);
 
       return imageType(buffer) !== null;
