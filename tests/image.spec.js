00fe5c502fa715eeb1ac6707b1cffddcccf47585
Sendy Halim <sendyhalim93@gmail.com>
2015-07-09 15:35:03 +0700

    Add tests for object input (image rule)

--- tests/image.spec.js
+++ tests/image.spec.js
@@ -22,4 +22,21 @@ describe('Image Validator', function() {
 
     expect(result.success).to.equals(true);
   });
+
+  it('should fail on non image type (object input)', function() {
+    var result = validator.validate(rules, {
+      imageInput: {path: 'README.md'}
+    });
+
+    expect(result.success).to.equals(false);
+    expect(result.messages.imageInput.image).to.equals('Image Input must be an image.');
+  });
+
+  it('should success on image type (object input)', function() {
+    var result = validator.validate(rules, {
+      imageInput: {path: 'tests/fixtures/dummyimg.jpeg'}
+    });
+
+    expect(result.success).to.equals(true);
+  });
 });
