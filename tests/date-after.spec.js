124a081c4c2c5382dc8afad75960a5823638dfb0
Sendy Halim <sendyhalim93@gmail.com>
2015-12-14 14:22:17 +0700

    Add more test cases for date-after:: rule

--- tests/date-after.spec.js
+++ tests/date-after.spec.js
@@ -25,5 +25,49 @@ describe('Date validator', function () {
       expect(result.success).to.equal(true);
       expect(err).to.not.have.property('vacationDate');
     });
+
+    it('should fail when input has the same date', function () {
+      var result = validator.validate(simpleRules, {
+        vacationDate: moment().format('DD/MM/YYYY')
+      });
+      var err = result.messages;
+
+      expect(result.success).to.equal(false);
+      expect(err).to.have.property('vacationDate');
+      expect(err.vacationDate['date-after:$1:$2']).to.equal('Vacation Date must greater than now.');
+    });
+  });
+
+  context('given a date-after rule with parameter 01-2014', function () {
+    var simpleRules = {
+      vacationDate: ['date-after:MM-YYYY:01-2014']
+    };
+
+    var getTestObject = function () {
+      var tomorrowDate = moment('02-2014', 'MM-YYYY');
+
+      return {
+        vacationDate: tomorrowDate,
+      };
+    };
+
+    it('should success', function () {
+      var result = validator.validate(simpleRules, getTestObject());
+      var err = result.messages;
+
+      expect(result.success).to.equal(true);
+      expect(err).to.not.have.property('vacationDate');
+    });
+
+    it('should fail when input has the same month', function () {
+      var result = validator.validate(simpleRules, {
+        vacationDate: moment('01-2014', 'MM-YYYY')
+      });
+      var err = result.messages;
+
+      expect(result.success).to.equal(false);
+      expect(err).to.have.property('vacationDate');
+      expect(err.vacationDate['date-after:$1:$2']).to.equal('Vacation Date must greater than 01-2014.');
+    });
   });
 });
