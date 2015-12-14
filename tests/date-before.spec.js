5bc0a552880ca41b37c2a7cf49f4b9057794848f
Sendy Halim <sendyhalim93@gmail.com>
2015-12-14 17:56:39 +0700

    Add test cases for relative  date

--- tests/date-before.spec.js
+++ tests/date-before.spec.js
@@ -7,7 +7,7 @@ var validator = require('../');
 describe('Date validator', function () {
   context('given a dateBefore rule with parameter `now`', function () {
     var simpleRules = {
-      pastVacationDate: ['dateBefore:DD/MM/YYYY:now']
+      pastVacationDate: ['dateBefore:DD/MM/YYYY:now:0']
     };
 
     var getTestObject = function () {
@@ -34,14 +34,14 @@ describe('Date validator', function () {
 
       expect(result.success).to.equal(false);
       expect(err).to.have.property('pastVacationDate');
-      expect(err.pastVacationDate).to.have.property('dateBefore:$1:$2')
+      expect(err.pastVacationDate).to.have.property('dateBefore:$1:$2:$3')
         .that.equals('Past Vacation Date must less than now.');
     });
   });
 
   context('given a dateBefore rule with parameter 01-2014', function () {
     var simpleRules = {
-      pastVacationDate: ['dateBefore:MM-YYYY:01-2014']
+      pastVacationDate: ['dateBefore:MM-YYYY:01-2014:0']
     };
 
     var getTestObject = function () {
@@ -68,8 +68,140 @@ describe('Date validator', function () {
 
       expect(result.success).to.equal(false);
       expect(err).to.have.property('pastVacationDate');
-      expect(err.pastVacationDate).to.have.property('dateBefore:$1:$2')
+      expect(err.pastVacationDate).to.have.property('dateBefore:$1:$2:$3')
         .that.equals('Past Vacation Date must less than 01-2014.');
+    });
+  });
+
+  context('given a dateBefore rule with parameter `now` and -365 days', function () {
+    var simpleRules = {
+      pastVacationDate: ['dateBefore:DD/MM/YYYY:now:-365']
+    };
+
+    var getTestObject = function () {
+      var pastDate = moment().subtract(366, 'days').format('DD/MM/YYYY');
+
+      return {
+        pastVacationDate: pastDate,
+      };
+    };
+
+    it('should success', function () {
+      var result = validator.validate(simpleRules, getTestObject());
+      var err = result.messages;
+
+      expect(result.success).to.equal(true);
+      expect(err).to.not.have.property('pastVacationDate');
+    });
+
+    it('should fail when input has the same date', function () {
+      var result = validator.validate(simpleRules, {
+        pastVacationDate: moment().subtract(365, 'days').format('DD/MM/YYYY')
+      });
+      var err = result.messages;
+
+      expect(result.success).to.equal(false);
+      expect(err).to.have.property('pastVacationDate');
+      expect(err.pastVacationDate).to.have.property('dateBefore:$1:$2:$3')
+        .that.equals('Past Vacation Date must less than now minus 365 days.');
+    });
+  });
+
+  context('given a dateBefore rule with parameter `now` and 40 days', function () {
+    var simpleRules = {
+      pastVacationDate: ['dateBefore:DD/MM/YYYY:now:40']
+    };
+
+    var getTestObject = function () {
+      var pastDate = moment().add(39, 'days').format('DD/MM/YYYY');
+
+      return {
+        pastVacationDate: pastDate,
+      };
+    };
+
+    it('should success', function () {
+      var result = validator.validate(simpleRules, getTestObject());
+      var err = result.messages;
+
+      expect(result.success).to.equal(true);
+      expect(err).to.not.have.property('pastVacationDate');
+    });
+
+    it('should fail when input has the same date', function () {
+      var result = validator.validate(simpleRules, {
+        pastVacationDate: moment().add(40, 'days').format('DD/MM/YYYY')
+      });
+      var err = result.messages;
+
+      expect(result.success).to.equal(false);
+      expect(err).to.have.property('pastVacationDate');
+      expect(err.pastVacationDate).to.have.property('dateBefore:$1:$2:$3')
+        .that.equals('Past Vacation Date must less than now plus 40 days.');
+    });
+  });
+
+  context('given a dateBefore rule with parameter `02-09` and -10 days', function () {
+    var simpleRules = {
+      pastVacationDate: ['dateBefore:DD-MM:02-09:-10']
+    };
+
+    var getTestObject = function () {
+      return {
+        pastVacationDate: '22-08',
+      };
+    };
+
+    it('should success', function () {
+      var result = validator.validate(simpleRules, getTestObject());
+      var err = result.messages;
+
+      expect(result.success).to.equal(true);
+      expect(err).to.not.have.property('pastVacationDate');
+    });
+
+    it('should fail when input has the same date', function () {
+      var result = validator.validate(simpleRules, {
+        pastVacationDate: '23-08'
+      });
+      var err = result.messages;
+
+      expect(result.success).to.equal(false);
+      expect(err).to.have.property('pastVacationDate');
+      expect(err.pastVacationDate).to.have.property('dateBefore:$1:$2:$3')
+        .that.equals('Past Vacation Date must less than 02-09 minus 10 days.');
+    });
+  });
+
+  context('given a dateBefore rule with parameter `12-09` and 10 days', function () {
+    var simpleRules = {
+      pastVacationDate: ['dateBefore:DD-MM:12-09:10']
+    };
+
+    var getTestObject = function () {
+      return {
+        pastVacationDate: '21-09',
+      };
+    };
+
+    it('should success', function () {
+      var result = validator.validate(simpleRules, getTestObject());
+      var err = result.messages;
+
+      expect(result.success).to.equal(true);
+      expect(err).to.not.have.property('pastVacationDate');
+    });
+
+    it('should fail when input has the same date', function () {
+      var result = validator.validate(simpleRules, {
+        pastVacationDate: '22-09'
+      });
+      var err = result.messages;
+
+      expect(result.success).to.equal(false);
+      expect(err).to.have.property('pastVacationDate');
+      expect(err.pastVacationDate).to.have.property('dateBefore:$1:$2:$3')
+        .that.equals('Past Vacation Date must less than 12-09 plus 10 days.');
     });
   });
 });
