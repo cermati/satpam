1a5ac298349d5cab3d2f946cd6ed768977747eb3
Sendy Halim <sendyhalim93@gmail.com>
2016-04-15 11:44:22 +0700

    Add InvalidValidationRuleParameter error type

---
+++ src/data-structures/errors.js
@@ -0,0 +1,20 @@
+import _ from 'lodash/fp';
+
+/**
+ * A class that represents an invalid validation rule pareameter
+ *
+ * @constructor
+ * @author Sendy Halim <sendyhalim93@gmail.com>
+ * @param message - The error message.
+ */
+function InvalidValidationRuleParameter(message) {
+  this.message = message;
+  this.name = 'InvalidValidationRuleParameter';
+
+  Error.captureStackTrace(this, InvalidValidationRuleParameter);
+}
+
+InvalidValidationRuleParameter.prototype = Object.create(Error.prototype);
+InvalidValidationRuleParameter.prototype.constructor = InvalidValidationRuleParameter;
+
+exports.InvalidValidationRuleParameter = InvalidValidationRuleParameter;
