47c71249689adf536546899cf51492fc7874f784
Sendy Halim <sendyhalim93@gmail.com>
2016-04-27 11:20:07 +0700

    Add data structure for `And` and `Or` required if

--- src/validators/required-if.js
+++ src/validators/required-if.js
@@ -1,9 +1,40 @@
+import _ from 'lodahs/fp';
 import required from './required';
+
+class And {
+  constructor(mappings) {
+    this.mappings = mappings;
+  }
+
+  satisfied(inputObj) {
+    const equalInput = function (mapping) {
+      return _.get(mapping.key, inputObj) === mapping.value;
+    };
+
+    return _.every(equalInput, this.mappings);
+  }
+}
+
+class Or {
+  constructor(mappings) {
+    this.mappings = mappings;
+  }
+
+  satisfied(inputObj) {
+    const equalInput = function (mapping) {
+      return _.get(mapping.key, inputObj) === mapping.value;
+    };
+
+    return _.some(equalInput, this.mappings);
+  }
+}
 
 const validate = (val, ruleObj, propertyName, inputObj) => {
   const targetProperty = ruleObj.params[0];
 
-  if (inputObj[targetProperty] === ruleObj.params[1]) {
+  if ((targetProperty instanceof And && targetProperty.satisifed(inputObj)) ||
+      (targetProperty instanceof Or && targetProperty.satisfied(inputObj)) ||
+      (inputObj[targetProperty] === ruleObj.params[1])) {
     return required.validate(val);
   }
 
