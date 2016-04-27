1211c03ce0409dbf632dafc6cb29232b29ecf942
Sendy Halim <sendyhalim93@gmail.com>
2016-04-27 14:07:03 +0700

    Add test cases for `And` conjunction data structure

--- test/validators/required-if.spec.js
+++ test/validators/required-if.spec.js
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import validator from '../../lib';
 import requiredIf from '../../lib/validators/required-if';
 
+
 describe('Required if validator', () => {
   context('with validator wrapper', () => {
     context('when given normal parameters', () => {
@@ -40,59 +41,6 @@ describe('Required if validator', () => {
     });
 
     context('when given `Or` structure as parameter', () => {
-      const testWithOr = or => {
-        const rules = {
-          address: [{
-            fullName: 'requiredIf:$1:$2',
-            params: [or]
-          }]
-        };
-
-        context('with satisfied parameter', () => {
-          it('should success with valid input', () => {
-            const result = validator.validate(rules, {
-              hasHome: 'yes',
-              address: 'wut'
-            });
-            const err = result.messages;
-
-            expect(result.success).to.equal(true);
-            expect(err).to.not.have.property('address');
-          });
-
-          it('should fail with invalid input', () => {
-            const result = validator.validate(rules, {
-              hasHome: 'yes'
-            });
-            const err = result.messages;
-
-            expect(result.success).to.equal(false);
-            expect(err.address).to.have.property('requiredIf:$1:$2')
-              .that.equals('Address field is required.');
-          });
-        });
-
-        context('with unsatisfied parameter', () => {
-          it('should success with different input dependency value', () => {
-            const result = validator.validate(rules, {
-              hasHome: 'yess'
-            });
-            const err = result.messages;
-
-            expect(result.success).to.equal(true);
-            expect(err).to.not.have.property('address');
-          });
-
-          it('should success with empty input', () => {
-            const result = validator.validate(rules, {});
-            const err = result.messages;
-
-            expect(result.success).to.equal(true);
-            expect(err).to.not.have.property('address');
-          });
-        });
-      };
-
       context('and it\'s an `Or` instance', () => {
         const or = new requiredIf.Or({
           hasHome: 'yes',
@@ -114,5 +62,137 @@ describe('Required if validator', () => {
         testWithOr(or);
       });
     });
+
+    context('when given `And` structure as parameter', () => {
+      context('and it\'s an `And` instance', () => {
+        const and = new requiredIf.And({
+          hasHome: 'yes',
+          hasApartment: 'yo'
+        });
+
+        testWithAnd(and);
+      });
+
+      context('and it\'s an object with type equals to and', () => {
+        const and = {
+          type: 'and',
+          mappings: {
+            hasHome: 'yes',
+            hasApartment: 'yo'
+          }
+        };
+
+        testWithAnd(and);
+      });
+    });
   });
 });
+
+function testWithAnd(and) {
+  const rules = {
+    address: [{
+      fullName: 'requiredIf:$1:$2',
+      params: [and]
+    }]
+  };
+
+  context('with satisfied parameter', () => {
+    it('should success with valid input', () => {
+      const result = validator.validate(rules, {
+        hasHome: 'yes',
+        hasApartment: 'yo',
+        address: 'wut'
+      });
+      const err = result.messages;
+
+      expect(result.success).to.equal(true);
+      expect(err).to.not.have.property('address');
+    });
+
+    it('should fail with invalid input', () => {
+      const result = validator.validate(rules, {
+        hasHome: 'yes',
+        hasApartment: 'yo'
+      });
+      const err = result.messages;
+
+      expect(result.success).to.equal(false);
+      expect(err.address).to.have.property('requiredIf:$1:$2')
+        .that.equals('Address field is required.');
+    });
+  });
+
+  context('with unsatisfied parameter', () => {
+    it('should success with different input dependency value', () => {
+      const result = validator.validate(rules, {
+        hasHome: 'yes',
+        hasApartment: 'yoo'
+      });
+      const err = result.messages;
+
+      expect(result.success).to.equal(true);
+      expect(err).to.not.have.property('address');
+    });
+
+    it('should success with empty input', () => {
+      const result = validator.validate(rules, {});
+      const err = result.messages;
+
+      expect(result.success).to.equal(true);
+      expect(err).to.not.have.property('address');
+    });
+  });
+}
+
+function testWithOr(or) {
+  const rules = {
+    address: [{
+      fullName: 'requiredIf:$1:$2',
+      params: [or]
+    }]
+  };
+
+  context('with satisfied parameter', () => {
+    it('should success with valid input', () => {
+      const result = validator.validate(rules, {
+        hasHome: 'yes',
+        address: 'wut'
+      });
+      const err = result.messages;
+
+      expect(result.success).to.equal(true);
+      expect(err).to.not.have.property('address');
+    });
+
+    it('should fail with invalid input', () => {
+      const result = validator.validate(rules, {
+        hasHome: 'yes'
+      });
+      const err = result.messages;
+
+      expect(result.success).to.equal(false);
+      expect(err.address).to.have.property('requiredIf:$1:$2')
+        .that.equals('Address field is required.');
+    });
+  });
+
+  context('with unsatisfied parameter', () => {
+    it('should success with different input dependency value', () => {
+      const result = validator.validate(rules, {
+        hasHome: 'yess'
+      });
+      const err = result.messages;
+
+      expect(result.success).to.equal(true);
+      expect(err).to.not.have.property('address');
+    });
+
+    it('should success with empty input', () => {
+      const result = validator.validate(rules, {});
+      const err = result.messages;
+
+      expect(result.success).to.equal(true);
+      expect(err).to.not.have.property('address');
+    });
+  });
+}
