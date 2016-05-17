a17905d311bfe381d4e9e2736d1c72d3efc9f809
Sendy Halim <sendyhalim93@gmail.com>
2016-05-17 12:07:32 +0700

    Add length rule

---
+++ src/validators/length.js
@@ -0,0 +1,15 @@
+import _ from 'lodash/fp';
+
+const validate = (val, ruleObj) => {
+  if (_.isUndefined(val) || _.isNull(val)) {
+    return true;
+  }
+
+  const valAsString = _.isFunction(val.toString) ? val.toString() : '';
+
+  return valAsString.length === ruleObj.params[0];
+};
+
+const message = '<%= propertyName %> must have length of <%= ruleParams[0] %> character(s).';
+
+export default {validate, message};
