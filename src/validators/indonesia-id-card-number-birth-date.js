51c9d4ae239baae8f5163ae7107ef07c18fee5c5
Sendy Halim <sendyhalim93@gmail.com>
2017-07-10 14:54:30 +0700

    Add indonesia id card number birth date validator

---
+++ src/validators/indonesia-id-card-number-birth-date.js
@@ -0,0 +1,22 @@
+import moment from 'moment';
+
+const validate = (value, ruleObj, propertyName, inputObj)  => {
+    // Do not validate if input value is falsy.
+  if (!value) {
+    return true;
+  }
+
+  // Birth date on indonesian id card starts at the 7th digit
+  // For example 012345[020993]3456 = 02-09-93 with format DD-MM-YYYY
+  // so basically we'll get the first 6 digits starting from the 7th digit (zero based index)
+  const birthDateFromIdCard = String(value).substr(6, 6);
+  const birthDateInputKey = ruleObj.params[0];
+  const birthDateInputFormat = ruleObj.params[1];
+  const birthDateValue = moment(inputObj[birthDateInputKey], birthDateInputFormat).format('DDMMYYYY');
+
+  return birthDateValue === birthDateFromIdCard;
+};
+
+const message = '<%= propertyName %> birth date number sequence does not match the given birth date.';
+
+export default {validate, message};
