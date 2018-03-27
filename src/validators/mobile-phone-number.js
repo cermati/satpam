d544703bacffc34a210657a3ceb7902a3cb8e261
febrian <febrianyuwono@gmail.com>
2018-03-27 15:06:11 +0700

    [Satpam] Update mobile phone number validation

--- src/validators/mobile-phone-number.js
+++ src/validators/mobile-phone-number.js
@@ -7,52 +7,63 @@ const mobilePhoneNumberRegex = /^(08|628|\+628)[0-9]{6,11}$/;
  * Sources
  * http://blog.mangpulsa.com/daftar-operator-seluler-gsm-indonesia/
  * http://www.kios-pulsa.com/article/daftar-prefix-nomor-operator-seluler-indonesia/
+ * http://pulsapul.blogspot.co.id/2011/11/0811-simpati.html
  */
 const prefixMap = {
-  '0811': true, // KartuHALO	Telkomsel
-  '0812': true, // SimPATI, KartuHALO	Telkomsel
-  '0813': true, // SimPATI, KartuHALO	Telkomsel
-  '0814': true, // Indosat 3,5G Broadband	Indosat (IndosatM2)
-  '0815': true, // Mentari lama, Matrix	Indosat
-  '0816': true, // Mentari, Matrix	Indosat
-  '0817': true, // XL Prabayar, XL Pascabayar	XL Axiata
-  '0818': true, // XL Prabayar, XL Pascabayar	XL Axiata
-  '0819': true, // XL Prabayar, XL Pascabayar	XL Axiata
-  '0821': true, // SimPATI Freedom	Telkomsel
-  '0822': true, // SimPATI (Kartu Facebook)	Telkomsel
+  '0811': true, // KartuHALO Telkomsel
+  '0812': true, // SimPATI, KartuHALO Telkomsel
+  '0813': true, // SimPATI, KartuHALO Telkomsel
+  '0814': true, // Indosat 3,5G Broadband Indosat (IndosatM2)
+  '0815': true, // Mentari lama, Matrix Indosat
+  '0816': true, // Mentari, Matrix Indosat
+  '0817': true, // XL Prabayar, XL Pascabayar XL Axiata
+  '0818': true, // XL Prabayar, XL Pascabayar XL Axiata
+  '0819': true, // XL Prabayar, XL Pascabayar XL Axiata
+  '0821': true, // SimPATI Freedom Telkomsel
+  '0822': true, // SimPATI (Kartu Facebook) Telkomsel
   '0823': true, // Kartu As
-  '0828': true, // Ceria	Sampoerna Telekom
-  '0831': true, // Axis	AXIS Telekom Indonesia
-  '0832': true, // Axis	AXIS Telekom Indonesia
-  '0833': true, // Axis	AXIS Telekom Indonesia
-  '0838': true, // Axis	AXIS Telekom Indonesia
-  '0851': true, // Nomor untu k Kartu AS (pengganti Flexi)
-  '0852': true, // Kartu As	Telkomsel
-  '0853': true, // Kartu As (Kartu Prima)	Telkomsel
-  '0855': true, // Matrix Auto	Indosat
-  '0856': true, // IM3	Indosat
-  '0857': true, // IM3	Indosat
-  '0858': true, // Mentari	Indosat
-  '0859': true, // XL Prabayar, XL Pascabayar	XL Axiata
-  '0868': true, // ByRU	PSN/ACeS
-  '0877': true, // XL Prabayar, Hauraa	XL Axiata
-  '0878': true, // XL Prabayar	XL Axiata
-  '0879': true, // XL Prabayar	XL Axiata
-  '0881': true, // smartfren	Smartfren Telecom
-  '0882': true, // smartfren	Smartfren Telecom
-  '0883': true, // Smartfren	Smartfren Telecom
-  '0884': true, // Smartfren	Smartfren Telecom
-  '0885': true, // Smartfren	Smartfren Telecom
-  '0886': true, // Smartfren	Smartfren Telecom
-  '0887': true, // smartfren	Smartfren Telecom
-  '0888': true, // smartfren	Smartfren Telecom
-  '0889': true, // smartfren	Smartfren Telecom
-  '0894': true, // 3	Hutchison Charoen Pokphand Telecom
-  '0895': true, // 3	Hutchison Charoen Pokphand Telecom
-  '0896': true, // 3	Hutchison Charoen Pokphand Telecom
-  '0897': true, // 3	Hutchison Charoen Pokphand Telecom
-  '0898': true, // 3	Hutchison Charoen Pokphand Telecom
-  '0899': true // 3	Hutchison Charoen Pokphand Telecom
+  '0828': true, // Ceria Sampoerna Telekom
+  '0830': true, // Axis AXIS Telekom Indonesia
+  '0831': true, // Axis AXIS Telekom Indonesia
+  '0832': true, // Axis AXIS Telekom Indonesia
+  '0833': true, // Axis AXIS Telekom Indonesia
+  '0834': true, // Axis AXIS Telekom Indonesia
+  '0835': true, // Axis AXIS Telekom Indonesia
+  '0836': true, // Axis AXIS Telekom Indonesia
+  '0837': true, // Axis AXIS Telekom Indonesia
+  '0838': true, // Axis AXIS Telekom Indonesia
+  '0839': true, // Axis AXIS Telekom Indonesia
+  '0851': true, // Nomor untuk Kartu AS (pengganti Flexi)
+  '0852': true, // Kartu As Telkomsel
+  '0853': true, // Kartu As (Kartu Prima) Telkomsel
+  '0855': true, // Matrix Auto Indosat
+  '0856': true, // IM3 Indosat
+  '0857': true, // IM3 Indosat
+  '0858': true, // Mentari Indosat
+  '0859': true, // XL Prabayar, XL Pascabayar XL Axiata
+  '0868': true, // ByRU PSN/ACeS
+  '0877': true, // XL Prabayar, Hauraa XL Axiata
+  '0878': true, // XL Prabayar XL Axiata
+  '0879': true, // XL Prabayar XL Axiata
+  '0881': true, // smartfren Smartfren Telecom
+  '0882': true, // smartfren Smartfren Telecom
+  '0883': true, // Smartfren Smartfren Telecom
+  '0884': true, // Smartfren Smartfren Telecom
+  '0885': true, // Smartfren Smartfren Telecom
+  '0886': true, // Smartfren Smartfren Telecom
+  '0887': true, // smartfren Smartfren Telecom
+  '0888': true, // smartfren Smartfren Telecom
+  '0889': true, // smartfren Smartfren Telecom
+  '0890': true, // 3 Hutchison Charoen Pokphand Telecom
+  '0891': true, // 3 Hutchison Charoen Pokphand Telecom
+  '0892': true, // 3 Hutchison Charoen Pokphand Telecom
+  '0893': true, // 3 Hutchison Charoen Pokphand Telecom
+  '0894': true, // 3 Hutchison Charoen Pokphand Telecom
+  '0895': true, // 3 Hutchison Charoen Pokphand Telecom
+  '0896': true, // 3 Hutchison Charoen Pokphand Telecom
+  '0897': true, // 3 Hutchison Charoen Pokphand Telecom
+  '0898': true, // 3 Hutchison Charoen Pokphand Telecom
+  '0899': true, // 3 Hutchison Charoen Pokphand Telecom
 };
 
 const normalizeMobilePhoneNumber = mobilePhoneNumber => {
