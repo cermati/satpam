25d83b32b3c06140c6b37432a71d9ab13de2fbb6
Albert Stevelino <albertstevelino@cermati.com>
2018-05-22 17:39:08 +0700

    Add translation for satpam

--- src/index.js
+++ src/index.js
@@ -412,6 +412,28 @@ class Validator {
   setValidationMessage(ruleName, message) {
     this.validation.messages[ruleName] = message;
   }
+
+  /**
+   * Set validation message using locale file
+   *
+   * @example
+   *   import satpam from 'satpam';
+   *   satpam.setValidationLocale(..);
+   *
+   * @param locale
+   */
+  setValidationLocale(locale) {
+    try {
+      const locale = require(`./locale/${R.toLower(locale)}`);
+
+      for (const key of R.keys(locale)) {
+        this.setValidationMessage(key, locale[key]);
+      }
+    } catch (error) {
+      throw new Error('Locale not found.');
+    }
+  }
+
 }
 
 /**
