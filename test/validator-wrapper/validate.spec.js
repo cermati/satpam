95ff6805b34f5b6c1777d9a61ea8a5e4bb51ba9f
Sendy Halim <sendyhalim93@gmail.com>
2020-07-01 18:07:52 +0700

    chore: Use a more predictable test logic

--- test/validator-wrapper/validate.spec.js
+++ test/validator-wrapper/validate.spec.js
@@ -388,7 +388,7 @@ describe('Validator.validate()', () => {
       it('should return formatted validation message', () => {
         const validationMessageparamsFormatterByRuleFullName = {
           'minValue:$1': ([ minParam ]) => {
-            return [Number(minParam).toLocaleString('en')];
+            return ['IDR ' + minParam];
           }
         };
 
@@ -410,7 +410,7 @@ describe('Validator.validate()', () => {
         const err = result.messages;
 
         expect(result.success).to.be.false;
-        expect(err.salary['minValue:$1']).to.deep.equal('Pendapatan must be greater than or equal to 3,900,888.');
+        expect(err.salary['minValue:$1']).to.deep.equal('Pendapatan must be greater than or equal to IDR 3900888.');
       });
     });
   });
