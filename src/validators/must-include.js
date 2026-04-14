0536500f8d9de38f131dda729191ef4bf5b43e32
gilbertzaini <gilbertzaini03@gmail.com>
2026-04-14 14:39:37 +0700

    ref(validator): change mustInclude to use prefix matching instead of exact match

--- src/validators/must-include.js
+++ src/validators/must-include.js
@@ -1,18 +1,24 @@
 import is from 'ramda/src/is';
 import isNil from 'ramda/src/isNil';
+import isEmpty from 'ramda/src/isEmpty';
 import all from 'ramda/src/all';
+import any from 'ramda/src/any';
+import startsWith from 'ramda/src/startsWith';
 
 const fullName = 'mustInclude:$1';
 
 const validate = (val, ruleObj) => {
-  if (isNil(val)) {
-    return true;
+  if (isEmpty(val) || isNil(val) || val.length < ruleObj.length) {
+    return false;
   }
 
   const valArray = is(Array, val) ? val : [val];
   const list = ruleObj.params[0];
 
-  return all(item => valArray.includes(item), list);
+return all(
+    prefix => any(startsWith(prefix), valArray), 
+    list
+  );
 };
 
 const message = '<%= propertyName %> must include all of <%= ruleParams[0] %>.';
--- test/validators/must-include.spec.js
+++ test/validators/must-include.spec.js
@@ -5,27 +5,15 @@ describe('MustInclude validator', () => {
   const objectRules = {
     item: [
       {
-        name: 'mustInclude', params: [['A', 'C']]
+        name: 'mustInclude', params: [['iphone', 'macbook']]
       }
     ]
   };
 
-  it('should success if an empty array is passed', () => {
-    const acceptedInputs = [];
-
-    acceptedInputs.forEach(function test(acceptedInput) {
-      const result = validator.validate(objectRules, {item: acceptedInput});
-      const err = result.messages;
-
-      expect(result.success).to.equal(true);
-      expect(err).to.not.have.property('item');  
-    })
-  })
-
   it('should success if inputs have the required items', () => {
     const acceptedInputs = [
-      ['A', 'C'],
-      ['A', 'B', 'C'],
+      ['iphone 17', 'macbook neo'],
+      ['iphone 17', 'charger', 'macbook neo'],
     ];
 
     acceptedInputs.forEach(function test(acceptedInput) {
@@ -39,8 +27,8 @@ describe('MustInclude validator', () => {
 
   it('should fail if inputs only have some of the required items', () => {
     const rejectedInputs = [
-      ['A', 'B'],
-      ['A', 'B', 'D'],
+      ['iphone 17', 'charger'],
+      ['iphone 17', 'charger', 'case'],
     ];
 
     rejectedInputs.forEach(function test(rejectedInput) {
@@ -49,14 +37,14 @@ describe('MustInclude validator', () => {
 
       expect(result.success).to.equal(false);
       expect(err).to.have.property('item');
-      expect(err.item['mustInclude:$1']).to.equal('Item must include all of A,C.');
+      expect(err.item['mustInclude:$1']).to.equal('Item must include all of iphone,macbook.');
     });
   });
 
   it('should fail if inputs have none of the required items', () => {
     const rejectedInputs = [
-      'B',
-      ['B', 'D'],
+      'charger',
+      ['charger', 'case'],
     ];
 
     rejectedInputs.forEach(function test(rejectedInput) {
@@ -65,8 +53,33 @@ describe('MustInclude validator', () => {
 
       expect(result.success).to.equal(false);
       expect(err).to.have.property('item');
-      expect(err.item['mustInclude:$1']).to.equal('Item must include all of A,C.');
+      expect(err.item['mustInclude:$1']).to.equal('Item must include all of iphone,macbook.');
     });
   });
 
+  it('should fail if an empty array is passed', () => {
+    const acceptedInputs = [];
+
+    acceptedInputs.forEach(function test(acceptedInput) {
+      const result = validator.validate(objectRules, {item: acceptedInput});
+      const err = result.messages;
+
+      expect(result.success).to.equal(false);
+      expect(err).to.have.property('item');
+      expect(err.item['mustInclude:$1']).to.equal('Item must include all of iphone,macbook.');
+    })
+  })
+
+  it('should fail if an array with items less than rule is passed', () => {
+    const acceptedInputs = ['iphone 17'];
+
+    acceptedInputs.forEach(function test(acceptedInput) {
+      const result = validator.validate(objectRules, {item: acceptedInput});
+      const err = result.messages;
+
+      expect(result.success).to.equal(false);
+      expect(err).to.have.property('item');
+      expect(err.item['mustInclude:$1']).to.equal('Item must include all of iphone,macbook.');
+    })
+  })
 });
