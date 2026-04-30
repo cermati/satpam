3bee5b5989b14fa27686ba5e85a889d666178c63
gilbertzaini <gilbertzaini03@gmail.com>
2026-04-30 16:01:44 +0700

    ref(validator): fix lint, optimize function call, use ramda on any-begin-with-case-insensitive

--- src/validators/any-begin-with-case-insensitive.js
+++ src/validators/any-begin-with-case-insensitive.js
@@ -1,17 +1,18 @@
 import isEmpty from 'ramda/src/isEmpty';
+import is from 'ramda/src/is';
 
 const fullName = 'anyBeginWithCaseInsensitive:$1';
+
+const sanitizeValue = (input) => {
+  return String(input).toLowerCase().trim();
+};
 
 const validate = (val, ruleObj) => {
   if (!val || isEmpty(val)) {
     return false;
   }
 
-  const sanitizeValue = (input) => {
-    return String(input).toLowerCase().trim();
-  }
-
-  const valArray = Array.isArray(val) ? val : [val];
+  const valArray = is(Array, val) ? val : [val];
   const prefixList = ruleObj.params[0].map(sanitizeValue);
 
   for (const item of valArray) {
